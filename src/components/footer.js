4be00e7f1c3439ec2be27b90bcfc64b63d00a4cb
Nicholas Black <57681918+nullxix@users.noreply.github.com>
2020-01-27 10:44:05 -0700

    prismitized footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,11 +1,25 @@
 import React from 'react'
+import {useStaticQuery, graphql} from 'gatsby'
 
-const Footer = () => {
+
+export default props => {
+
+    const _data = useStaticQuery(graphql`
+        query FooterQuery {
+            prismicFooter {
+                data {
+                    footerText {
+                        text
+                    }
+                }
+            }
+        }
+    `)
+    const data = _data.prismicFooter.data //so we don't have to type query.data.field.data.query.data.thing.text.otherthing
+    
     return (
         <footer>
-            <p>Created by Vschool, © 2020</p>
+            <p>{data.footerText.text}</p>
         </footer>
     )
 }
-
-export default Footer
