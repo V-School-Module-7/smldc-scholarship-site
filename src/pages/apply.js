0c8f1f5e2a9df20311d34a571666b2d25b3da2a9
Erik Flores <erik.flores@colorado.edu>
2020-02-11 17:31:35 -0700

    query not complete

--- src/pages/apply.js
+++ src/pages/apply.js
@@ -1,26 +1,43 @@
 import React from'react'
+import {useStaticQuery, graphql} from 'gatsby'
 
 import Footer from '../components/footer'
 
 const Apply = () => {
-    return (
-        <div>
-            <h1>HOW TO APPLY</h1>
-            <h3>Deadline for Applications is Friday, February 7th, 2020.</h3>
-            <h3>In order to apply for the Skip Morgan LDC Scholarship, you must meet all of the following criteria:</h3>
-            <ol>
-                <li>Be a former Utah American Red Cross LDC Delegate</li>
-                <li>Have been accepted at a higher education institution (Including trade schools)</li>
-                <li>Have 70 hours of service with at least 20 at the American Red Cross (must be logged in Volunteer Connection) within the previous calendar year.</li>
-                <li>Complete the online application (link below)</li>
-                <li>Maintain a minimum cumulative GPA of 2.75</li>
-            </ol>
+    const data = useStaticQuery(graphql `
+    query applyPage {
+        allPrismicApplicationPage {
+          edges {
+            node {
+              data {
+                title {
+                  text
+                }
+              }
+            }
+          }
+        }
+      }
+      `)
+//     return (
+//         <div>
+//             <h1>HOW TO APPLY</h1>
+//             <h3>Deadline for Applications is Friday, February 7th, 2020.</h3>
+//             <h3>In order to apply for the Skip Morgan LDC Scholarship, you must meet all of the following criteria:</h3>
+//             <ol>
+//                 {data.allPrismicApplicationPage.edges.node.}
+//                 <li>Be a former Utah American Red Cross LDC Delegate</li>
+//                 <li>Have been accepted at a higher education institution (Including trade schools)</li>
+//                 <li>Have 70 hours of service with at least 20 at the American Red Cross (must be logged in Volunteer Connection) within the previous calendar year.</li>
+//                 <li>Complete the online application (link below)</li>
+//                 <li>Maintain a minimum cumulative GPA of 2.75</li>
+//             </ol>
 
-            <h4>IF YOU MEET THIS CRITERIA, APPLY HERE</h4>
-            <h4>AS PART OF THE APPLICATION PROCESS, A SUMMARY OF SERVICE HOURS AND ACTIVITIES IS REQUESTED. CLICK HERE FOR A FORM SAMPLE</h4>
-        <Footer />
-        </div>
-    )
-}
+//             <h4>IF YOU MEET THIS CRITERIA, APPLY HERE</h4>
+//             <h4>AS PART OF THE APPLICATION PROCESS, A SUMMARY OF SERVICE HOURS AND ACTIVITIES IS REQUESTED. CLICK HERE FOR A FORM SAMPLE</h4>
+//         <Footer />
+//         </div>
+//     )
+// }
 
 export default Apply
