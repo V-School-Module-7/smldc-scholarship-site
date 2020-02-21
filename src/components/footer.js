fbdeea784a963a854f2d308301e8882a6624e642
Erik Flores <erik.flores@colorado.edu>
2020-02-20 17:49:52 -0700

    update

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,12 +1,25 @@
 import React from 'react'
+import {useStaticQuery, graphql} from 'gatsby'
 
 
-const Footer = () => {
+export default props => {
+
+    const _data = useStaticQuery(graphql`
+        query FooterQuery {
+            prismicFooter {
+                data {
+                    footerText {
+                        text
+                    }
+                }
+            }
+        }
+    `)
+    const data = _data.prismicFooter.data //so we don't have to type query.data.field.data.query.data.thing.text.otherthing
+    
     return (
         <footer>
-            <p>© 2020 SkipMorganLDCScholarship. All Rights Reserved</p>
+            <p>{data.footerText.text}</p>
         </footer>
     )
 }
-
-export default Footer
