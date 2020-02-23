700da0e8e3f82bc8d6aed7a3e40b06089686f377
Salvatore <salvatore.dep@gmail.com>
2020-02-23 12:24:09 -0500

    connected pages to header links, added layout to multiple pages

--- src/pages/board.js
+++ src/pages/board.js
@@ -3,13 +3,14 @@ import Footer from '../components/footer.js'
 import BoardMember from '../components/boardMember.js'
 import {graphql} from 'gatsby'
 import globalStyles from '../styles/global.module.css'
+import Layout from '../components/layout'
 
 export default props => {
 
     const boardData = props.data.allPrismicBoardmember.edges.map(edge => edge.node)
 
     return (
-        <>
+        <Layout>
             <div className={globalStyles.title}>Board of Directors</div>
             <div>   
                 {boardData.map(datum => <BoardMember {...datum}/>)}
@@ -17,8 +18,7 @@ export default props => {
             <div>
                  Stuff about our organization and our board! We value true love, high quality sushi, and 3-day work weeks. On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains.
             </div>
-            <Footer/>
-        </>
+        </Layout>
     )
 }
 
