69dd816eef8d9cda5625c74af8fdd4a28557638f
Nicholas Black <57681918+nullxix@users.noreply.github.com>
2020-03-06 18:30:58 -0700

    board members implemented as prismic group

--- src/pages/board.js
+++ src/pages/board.js
@@ -1,48 +1,55 @@
 import React from 'react'
-import Footer from '../components/footer.js'
 import BoardMember from '../components/boardMember.js'
 import {graphql} from 'gatsby'
 import globalStyles from '../styles/global.module.css'
 import Layout from '../components/layout'
+import SliceRenderer from '../components/slices/index.js'
 
 export default props => {
-
-    const boardData = props.data.allPrismicBoardmember.edges.map(edge => edge.node)
-
+    const {board_members, body} = props.data.prismicBoard.data
     return (
         <Layout>
             <div className={globalStyles.title}>Board of Directors</div>
             <div>   
-                {boardData.map(datum => <BoardMember {...datum}/>)}
+                {board_members.map(datum => <BoardMember {...datum}/>)}
             </div>
-            <div>
-                 Stuff about our organization and our board! We value true love, high quality sushi, and 3-day work weeks. On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains.
-            </div>
+            <SliceRenderer slices={data.body}/>
         </Layout>
     )
 }
 
 export const pageQuery = graphql`
-query BoardQuery {
-    allPrismicBoardmember {
-      edges {
-        node {
-          data {
-            headshot {
+  {
+    prismicBoard {
+      data {
+        board_members {
+          headshot {
+            url
+          }
+          name
+          position
+        }
+        body {
+          items {
+            float_right
+            image {
               url
             }
-            learn_more {
-              url
-            }
-            name {
-              text
-            }
-            position {
+            paragraph {
+              raw {
+                spans {
+                  end
+                  start
+                  type
+                }
+                text
+                type
+              }
               text
             }
           }
         }
       }
     }
-  }  
+  }
 `
